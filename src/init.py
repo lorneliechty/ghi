a50e0977a2fa66c23a391b61f622650137358cbe
lorne <liechty.lorne@gmail.com>
2012-05-05 00:21:32 -0500

    Added issues directory to ghi directory tree

--- src/init.py
+++ src/init.py
@@ -12,10 +12,14 @@ GIT_ROOT = subprocess.Popen(
 	stderr=subprocess.STDOUT, 
 	close_fds=True).stdout.read().strip()
 
-# Check to see if the .ghi directory has already been created
+# Check to see if the .ghi directories have already been created
 # If it doesn't exist, create it.
 GHI_DIR = GIT_ROOT + '/.ghi'
+ISSUES_DIR = GHI_DIR + '/issues'
 if os.path.isdir(GHI_DIR) == False:
 	os.makedirs(GHI_DIR)
+	os.makedirs(ISSUES_DIR)
+elif os.path.isdir(ISSUES_DIR) == False:
+	os.makedirs(ISSUES_DIR)
 else:
 	print "This git already has issues."
