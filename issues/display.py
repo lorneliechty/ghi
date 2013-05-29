42fb94900dec1cf13297fe81c5a0330bc2e63d3f
lorne <liechty.lorne@gmail.com>
2013-05-29 16:03:29 -0500

    Updating the copyright dates in the license headers

--- issues/display.py
+++ issues/display.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2012 Lorne Liechty
+# Copyright (C) 2012-2013 Lorne Liechty
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- issues/identifiers.py
+++ issues/identifiers.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 #
-# Copyright (C) 2012 Lorne Liechty
+# Copyright (C) 2012-2013 Lorne Liechty
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
